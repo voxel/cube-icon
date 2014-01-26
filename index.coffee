0766cc3c8259a3dd27d9a36c5c36dde4dde534b2
deathcap <deathcap@gmx.co.uk>
2014-01-25 18:38:31 -0800

    All cube faces

--- index.coffee
+++ index.coffee
@@ -5,13 +5,40 @@ class CubeIcon
   constructor: (opts) ->
 
     @container = document.createElement 'div'
+
+    s = 16
+    dz = s / 2
+
     @container.setAttribute 'style', "
+-webkit-transform: translateZ( -100px) rotateX(-45deg) rotateY(-45deg);
+position: absolute;
 -webkit-transform-style: preserve-3d;
--webkit-transform: translateX(6px) translateY(6px) rotateX(-15deg) rotateY(45deg);
-width: 16px;
-height: 16px;
+"
+
+    faceTransforms = { # http://desandro.github.io/3dtransforms/docs/cube.html
+      front:  "rotateY(   0deg ) translateZ( #{dz}px )"
+      back:   "rotateX( 180deg ) translateZ( #{dz}px )"
+      right:  "rotateY(  90deg ) translateZ( #{dz}px )"
+      left:   "rotateY( -90deg ) translateZ( #{dz}px )"
+      top:    "rotateX(  90deg ) translateZ( #{dz}px )"
+      bottom: "rotateX( -90deg ) translateZ( #{dz}px )"
+    }
+
+    faces = Object.keys(faceTransforms)
+    for faceName, i in faces
+      face = document.createElement 'div'
+      face.setAttribute 'style', "
+-webkit-transform-style: preserve-3d;
+-webkit-transform: #{faceTransforms[faceName]};
+position: absolute;
+border: 0.5px solid black;
+width: #{s}px;
+height: #{s}px;
 background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAb0lEQVQ4y2OYmWb8/84U7/8gGhfGJg8TY8AlQchQGGYgRhGyYegGM5BiK7o8iM9Air+xegGbqcQahuICYm1EV8tArI24LGAgN/5R0gEl4cBAbujDA5EYDQQTErGKceYFcqMQwwXEBCZRYUDIIGQ+AHmcSKuZbPIVAAAAAElFTkSuQmCC);
 "
+      face.textContent = i
+
+      @container.appendChild face
 
 
 document.body.appendChild new CubeIcon().container
--- index.js
+++ index.js
@@ -8,8 +8,27 @@
 
   CubeIcon = (function() {
     function CubeIcon(opts) {
+      var dz, face, faceName, faceTransforms, faces, i, s, _i, _len;
       this.container = document.createElement('div');
-      this.container.setAttribute('style', "-webkit-transform-style: preserve-3d;-webkit-transform: translateX(6px) translateY(6px) rotateX(-15deg) rotateY(45deg);width: 16px;height: 16px;background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAb0lEQVQ4y2OYmWb8/84U7/8gGhfGJg8TY8AlQchQGGYgRhGyYegGM5BiK7o8iM9Air+xegGbqcQahuICYm1EV8tArI24LGAgN/5R0gEl4cBAbujDA5EYDQQTErGKceYFcqMQwwXEBCZRYUDIIGQ+AHmcSKuZbPIVAAAAAElFTkSuQmCC);");
+      s = 16;
+      dz = s / 2;
+      this.container.setAttribute('style', "-webkit-transform: translateZ( -100px) rotateX(-45deg) rotateY(-45deg);position: absolute;-webkit-transform-style: preserve-3d;");
+      faceTransforms = {
+        front: "rotateY(   0deg ) translateZ( " + dz + "px )",
+        back: "rotateX( 180deg ) translateZ( " + dz + "px )",
+        right: "rotateY(  90deg ) translateZ( " + dz + "px )",
+        left: "rotateY( -90deg ) translateZ( " + dz + "px )",
+        top: "rotateX(  90deg ) translateZ( " + dz + "px )",
+        bottom: "rotateX( -90deg ) translateZ( " + dz + "px )"
+      };
+      faces = Object.keys(faceTransforms);
+      for (i = _i = 0, _len = faces.length; _i < _len; i = ++_i) {
+        faceName = faces[i];
+        face = document.createElement('div');
+        face.setAttribute('style', "-webkit-transform-style: preserve-3d;-webkit-transform: " + faceTransforms[faceName] + ";position: absolute;border: 0.5px solid black;width: " + s + "px;height: " + s + "px;background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAb0lEQVQ4y2OYmWb8/84U7/8gGhfGJg8TY8AlQchQGGYgRhGyYegGM5BiK7o8iM9Air+xegGbqcQahuICYm1EV8tArI24LGAgN/5R0gEl4cBAbujDA5EYDQQTErGKceYFcqMQwwXEBCZRYUDIIGQ+AHmcSKuZbPIVAAAAAElFTkSuQmCC);");
+        face.textContent = i;
+        this.container.appendChild(face);
+      }
     }
 
     return CubeIcon;
